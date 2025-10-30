15a277b47a3e4c9e4b17a8dadc06b50c0ef4b5ee
haoking78 <haoking.inc@gmail.com>
2025-10-31 02:12:16 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,3 @@
-
-const CACHE = 'ufo-app-v1.0';
-const CORE = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))); });
-self.addEventListener('activate', e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))); self.clients.claim(); });
-self.addEventListener('fetch', e=>{
-  const url = new URL(e.request.url);
-  if(url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
-      const copy = res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return res;
-    })));
-  }
-});
+// 極簡 SW：不做持久快取，每次啟用即接管，確保讀最新檔
+self.addEventListener('install', (event) => { self.skipWaiting(); });
+self.addEventListener('activate', (event) => { event.waitUntil(self.clients.claim()); });
