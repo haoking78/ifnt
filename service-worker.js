0da968907d3dd4e2352cfb263971df9f9099da6e
haoking78 <haoking.inc@gmail.com>
2025-10-31 09:19:45 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,2 @@
-// placeholder
+self.addEventListener('install',e=>{e.waitUntil(caches.open('ifnt-v1').then(c=>c.addAll(['./','./index.html','./manifest.json','./app_icon_192.png','./app_icon_512.png'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
