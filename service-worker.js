8f016c7670d75a0c919c92178bcd6ff1e6b7daf5
haoking78 <haoking.inc@gmail.com>
2025-10-31 15:04:18 +0800

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME='ifnt-cache-v6.4.1-20251031-3';
-const ASSETS=['./','./index.html','./app.js','./manifest.json','./app_icon_192.png','./app_icon_512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
